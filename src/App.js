5305660ccdd6fcfdc5f6a21281d336a88cd788f8
바키찬 <ckstmznf@naver.com>
2023-12-05 19:55:52 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
 import './App.css';
+
+import rank from "./data/rank"
+import {ResponsiveLine} from '@nivo/line'
+import {useEffect, useState} from "react";
+
+function RankGraph() {
+  const [parentWidth, setParentWidth] = useState(window.innerWidth)
+  const [startYear, setStartYear] = useState(0)
+  const [endYear, setEndYear] = useState(parentWidth)
+  const [selectTeam, setSelectTeam] = useState(null)
+  const [isStartMove, setIsStartMove] = useState(false)
+  const [isEndMove, setIsEndMove] = useState(false)
+
+  // let d = []
+  // rank.forEach((value) => {
+  //   if (selectTeam == null || value.id === selectTeam) {
+  //     d.push(value)
+  //   } else {
+  //     const v = {...value}
+  //     v.data = []
+  //     d.push(v)
+  //   }
+  // })
+
+
+  useEffect(() => {
+    const handelMouseMove = (e) => {
+      if (isStartMove) {
+        if (!e.buttons) {
+          setIsStartMove(false)
+          return
+        }
+
+        setStartYear(Math.max(startYear + e.movementX, 0))
+        if(startYear + 40 > endYear - 40){
+          setStartYear(endYear - 80)
+          setIsStartMove(false)
+          return
+        }
+      }
+
+      if (isEndMove) {
+        if (!e.buttons) {
+          setIsEndMove(false)
+          return
+        }
+
+        setEndYear(Math.min(endYear + e.movementX, parentWidth))
+        if(endYear - 40 < startYear + 40){
+          setEndYear(startYear + 80)
+          setIsEndMove(false)
+          return
+        }
+      }
+    }
+    document.addEventListener("mousemove", handelMouseMove)
+
+    return () => {
+      document.removeEventListener("mousemove", handelMouseMove)
+    }
+  })
+
+  return (
+    <>
+      <div className="year-selector-wrap" onResize={(e) => {
+        setParentWidth(e.currentTarget.clientWidth)
+      }}>
+        <div className="year-start arrow"
+             style={{left: startYear, width: 40}}
+             onMouseDown={() => {
+               setIsStartMove(true)
+             }}
+             onMouseUp={() => {
+               setIsStartMove(false)
+             }}
+        ></div>
+        <div className="year-end arrow"
+             style={{left: endYear - 40, width: 40}}
+             onMouseDown={() => {
+               setIsEndMove(true)
+             }}
+             onMouseUp={() => {
+               setIsEndMove(false)
+             }}
+        ></div>
+        <div className="year-bar"
+             style={{
+               left: startYear,
+               width: endYear - startYear
+             }}
+        ></div>
+      </div>
+      {/*{isStartMove ? <span>참</span> : <span>거짓</span>}, {isEndMove ? <span>참</span> : <span>거짓</span>}*/}
+
+      {/*<ResponsiveLine*/}
+      {/*  isInteractive*/}
+      {/*  useMesh*/}
+      {/*  data={d}*/}
+      {/*  colors={d.map((value) => {return value.color})}*/}
+      {/*  margin={{top : 50, right : 100, bottom : 50, left : 50}}*/}
+      {/*  xScale={{*/}
+      {/*    type : "linear",*/}
+      {/*    min : startYear,*/}
+      {/*    max : endYear,*/}
+      {/*  }}*/}
+      {/*  yScale={{*/}
+      {/*    type: 'linear',*/}
+      {/*    min: 1,*/}
+      {/*    max: 10,*/}
+      {/*    reverse: true*/}
+      {/*  }}*/}
+      {/*  legends={[*/}
+      {/*    {*/}
+      {/*      anchor: 'bottom-right',*/}
+      {/*      direction: 'column',*/}
+      {/*      justify: false,*/}
+      {/*      translateX: 100,*/}
+      {/*      translateY: 0,*/}
+      {/*      itemsSpacing: 0,*/}
+      {/*      itemDirection: 'left-to-right',*/}
+      {/*      itemWidth: 80,*/}
+      {/*      itemHeight: 20,*/}
+      {/*      itemOpacity: 0.75,*/}
+      {/*      symbolSize: 12,*/}
+      {/*      symbolShape: 'circle',*/}
+      {/*      symbolBorderColor: 'rgba(0, 0, 0, .5)',*/}
+      {/*      effects: [*/}
+      {/*        {*/}
+      {/*          on: 'hover',*/}
+      {/*          style: {*/}
+      {/*            itemBackground: 'rgba(0, 0, 0, .03)',*/}
+      {/*            itemOpacity: 1*/}
+      {/*          }*/}
+      {/*        }*/}
+      {/*      ],*/}
+      {/*      onClick : (datum) => {*/}
+      {/*        if(selectTeam === datum.id)*/}
+      {/*          setSelectTeam(null)*/}
+      {/*        else*/}
+      {/*          setSelectTeam(datum.id)*/}
+      {/*      }*/}
+      {/*    }*/}
+      {/*  ]}*/}
+
+      {/*/>*/}
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RankGraph/>
     </div>
   );
 }
---
+++ src/data/team.js
@@ -0,0 +1,49 @@
+export default [
+  {'team_name': '롯데', 'team_id': 0, 'color1': '#041E42', 'old_name': ['롯데']},
+  {
+    'team_name': 'KIA',
+    'team_id': 1,
+    'color1': '#EA0029',
+    'old_name': ['해태', 'KIA']
+  },
+  {'team_name': '삼성', 'team_id': 2, 'color1': '#074CA1', 'old_name': ['삼성']},
+  {
+    'team_name': 'LG',
+    'team_id': 3,
+    'color1': '#C30452',
+    'old_name': ['MBC', 'LG']
+  },
+  {
+    'team_name': '두산',
+    'team_id': 4,
+    'color1': '#131230',
+    'old_name': ['OB', '두산']
+  },
+  {
+    'team_name': '한화',
+    'team_id': 5,
+    'color1': '#FF6600',
+    'old_name': ['빙그레', '한화']
+  },
+  {
+    'team_name': 'SSG',
+    'team_id': 6,
+    'color1': '#CE0E2D',
+    'old_name': ['SK', 'SSG']
+  },
+  {
+    'team_name': '키움',
+    'team_id': 7,
+    'color1': '#570514',
+    'old_name': ['우리', '넥센', '히어로즈', '키움']
+  },
+  {'team_name': 'NC', 'team_id': 8, 'color1': '#315288', 'old_name': ['NC']},
+  {'team_name': 'KT', 'team_id': 9, 'color1': '#000000', 'old_name': ['KT']},
+  {
+    'team_name': '현대',
+    'team_id': 10,
+    'color1': '#007f55',
+    'old_name': ['삼미', '청보', '태평양', '현대']
+  },
+  {'team_name': '쌍방울', 'team_id': 11, 'color1': '#ffc81e', 'old_name': ['쌍방울']}
+]
