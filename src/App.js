c23cbc994e060d970671679ce9ced980c3ab5dcf
바키찬 <ckstmznf@naver.com>
2023-12-09 00:08:56 +0900

    remove : 그래프 연도 선택기 리셋

--- src/App.js
+++ src/App.js
@@ -2,149 +2,100 @@ import './App.css';
 
 import rank from "./data/rank"
 import {ResponsiveLine} from '@nivo/line'
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 function RankGraph() {
-  const [parentWidth, setParentWidth] = useState(window.innerWidth)
-  const [startYear, setStartYear] = useState(0)
-  const [endYear, setEndYear] = useState(parentWidth)
   const [selectTeam, setSelectTeam] = useState(null)
-  const [isStartMove, setIsStartMove] = useState(false)
-  const [isEndMove, setIsEndMove] = useState(false)
 
-  // let d = []
-  // rank.forEach((value) => {
-  //   if (selectTeam == null || value.id === selectTeam) {
-  //     d.push(value)
-  //   } else {
-  //     const v = {...value}
-  //     v.data = []
-  //     d.push(v)
-  //   }
-  // })
-
-
-  useEffect(() => {
-    const handelMouseMove = (e) => {
-      if (isStartMove) {
-        if (!e.buttons) {
-          setIsStartMove(false)
-          return
-        }
-
-        setStartYear(Math.max(startYear + e.movementX, 0))
-        if(startYear + 40 > endYear - 40){
-          setStartYear(endYear - 80)
-          setIsStartMove(false)
-          return
-        }
-      }
-
-      if (isEndMove) {
-        if (!e.buttons) {
-          setIsEndMove(false)
-          return
-        }
-
-        setEndYear(Math.min(endYear + e.movementX, parentWidth))
-        if(endYear - 40 < startYear + 40){
-          setEndYear(startYear + 80)
-          setIsEndMove(false)
-          return
-        }
-      }
-    }
-    document.addEventListener("mousemove", handelMouseMove)
-
-    return () => {
-      document.removeEventListener("mousemove", handelMouseMove)
+  let d = []
+  rank.forEach((value) => {
+    if (selectTeam == null || value.id === selectTeam) {
+      d.push(value)
+    } else {
+      const v = {...value}
+      v.data = []
+      d.push(v)
     }
   })
 
   return (
     <>
-      <div className="year-selector-wrap" onResize={(e) => {
-        setParentWidth(e.currentTarget.clientWidth)
-      }}>
-        <div className="year-start arrow"
-             style={{left: startYear, width: 40}}
-             onMouseDown={() => {
-               setIsStartMove(true)
-             }}
-             onMouseUp={() => {
-               setIsStartMove(false)
-             }}
-        ></div>
-        <div className="year-end arrow"
-             style={{left: endYear - 40, width: 40}}
-             onMouseDown={() => {
-               setIsEndMove(true)
-             }}
-             onMouseUp={() => {
-               setIsEndMove(false)
-             }}
-        ></div>
-        <div className="year-bar"
-             style={{
-               left: startYear,
-               width: endYear - startYear
-             }}
-        ></div>
-      </div>
-      {/*{isStartMove ? <span>참</span> : <span>거짓</span>}, {isEndMove ? <span>참</span> : <span>거짓</span>}*/}
+      <ResponsiveLine
+        isInteractive
+        useMesh
+        data={d}
+        colors={d.map((value) => {return value.color})}
+        margin={{top : 50, right : 100, bottom : 50, left : 50}}
+        xScale={{
+          type : "linear",
+          min : 1981,
+          max : 2023,
+        }}
+        yScale={{
+          type: 'linear',
+          min: 1,
+          max: 10,
+          reverse: true
+        }}
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1
+                }
+              }
+            ],
+            onClick : (datum) => {
+              if(selectTeam === datum.id)
+                setSelectTeam(null)
+              else
+                setSelectTeam(datum.id)
+            }
+          }
+        ]}
 
-      {/*<ResponsiveLine*/}
-      {/*  isInteractive*/}
-      {/*  useMesh*/}
-      {/*  data={d}*/}
-      {/*  colors={d.map((value) => {return value.color})}*/}
-      {/*  margin={{top : 50, right : 100, bottom : 50, left : 50}}*/}
-      {/*  xScale={{*/}
-      {/*    type : "linear",*/}
-      {/*    min : startYear,*/}
-      {/*    max : endYear,*/}
-      {/*  }}*/}
-      {/*  yScale={{*/}
-      {/*    type: 'linear',*/}
-      {/*    min: 1,*/}
-      {/*    max: 10,*/}
-      {/*    reverse: true*/}
-      {/*  }}*/}
-      {/*  legends={[*/}
-      {/*    {*/}
-      {/*      anchor: 'bottom-right',*/}
-      {/*      direction: 'column',*/}
-      {/*      justify: false,*/}
-      {/*      translateX: 100,*/}
-      {/*      translateY: 0,*/}
-      {/*      itemsSpacing: 0,*/}
-      {/*      itemDirection: 'left-to-right',*/}
-      {/*      itemWidth: 80,*/}
-      {/*      itemHeight: 20,*/}
-      {/*      itemOpacity: 0.75,*/}
-      {/*      symbolSize: 12,*/}
-      {/*      symbolShape: 'circle',*/}
-      {/*      symbolBorderColor: 'rgba(0, 0, 0, .5)',*/}
-      {/*      effects: [*/}
-      {/*        {*/}
-      {/*          on: 'hover',*/}
-      {/*          style: {*/}
-      {/*            itemBackground: 'rgba(0, 0, 0, .03)',*/}
-      {/*            itemOpacity: 1*/}
-      {/*          }*/}
-      {/*        }*/}
-      {/*      ],*/}
-      {/*      onClick : (datum) => {*/}
-      {/*        if(selectTeam === datum.id)*/}
-      {/*          setSelectTeam(null)*/}
-      {/*        else*/}
-      {/*          setSelectTeam(datum.id)*/}
-      {/*      }*/}
-      {/*    }*/}
-      {/*  ]}*/}
-
-      {/*/>*/}
+      />
     </>
+  )
+}
+
+let btnRenderCount = 0
+
+function CountUpBtn({onClick, count}) {
+  console.log("버튼 랜더링", btnRenderCount++)
+  return (
+    <button onClick={onClick}>{count.num}</button>
+  )
+}
+
+function UserCallTest() {
+  const [count, setCount] = useState({num: 0})
+
+  const incressment = useCallback(() => {
+    setCount({num: count.num + 1})
+  }, [count])
+
+  return (
+    <div>
+      <h1>{count.num}</h1>
+      <CountUpBtn onClick={incressment} count={count}/>
+    </div>
   )
 }
 
@@ -152,6 +103,7 @@ function App() {
   return (
     <div className="App">
       <RankGraph/>
+      {/*<UserCallTest/>*/}
     </div>
   );
 }
